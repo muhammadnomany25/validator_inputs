ee4b679e06f5eed2bfb65933850c493c360a1ba4
Muhammad Noamany <muhammadnoamany@gmail.com>
2021-12-12 17:44:52 +0200

    ## First commit ## Create Custom Text Input view that make it possible to have a input field type and validates its inputs according to it

---
+++ asgainputs/src/main/java/com/asgatech/asgainputs/CustomInputField.kt
@@ -0,0 +1,282 @@
+package com.asgatech.asgainputs
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.TypedArray
+import android.graphics.Color
+import android.text.InputType
+import android.util.AttributeSet
+import android.util.Log
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.widget.doOnTextChanged
+import com.asgatech.asgainputs.utils.ValidationUtil
+import com.google.android.material.textfield.TextInputLayout
+
+/**
+ * @Author: Muhammad Noamany
+ * @Date: 12/12/2021
+ * @Email: muhammadnoamany@gmail.com
+ */
+class CustomInputField : TextInputLayout {
+    private lateinit var inputEditText: AppCompatEditText
+    private var errorColor: ColorStateList? = null
+    private var inputHint: String = ""
+    private var errorText: String = ""
+    private var inputFieldType: Int = -1
+    private var minLength: Int = 1
+    private var maxLength: Int = 100
+    private var hasDot: Boolean = false
+    private var hasUnderScore: Boolean = false
+    private var hasSpecialCharacter: Boolean = false
+    private var hasNumbers: Boolean = false
+    private var hasCapitalLetters: Boolean = false
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        val typedAttributeSet = context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CustomInputField,
+            0,
+            0
+        )
+        try {
+            readXmlAttr(typedAttributeSet)
+            setBoxErrorColor(errorColor)
+        } finally {
+            typedAttributeSet.recycle()
+        }
+
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    /**
+     * Fetch attr from xml view or init with the default values
+     * inputHint => the hint of the contained edit text
+     * errorText => the text of the validation error
+     * input field type => type of the field (username, password, email, egyPhone, ksaPhone, ksaNationalId, egyNationalId, Payment Cards(Visa, Master , Amex) and ipAddress )
+     * errorColor => maximum length of username or password
+     * minLength => minimum length of username or password
+     * maxLength => maximum length of username or password
+     * hasDot => the capability of having [.] for username or password
+     * hasUnderScore => the capability of having [_]  for username or password
+     * hasSpecialCharacter => the capability of having [!@#$]  for username or password
+     * hasCapitalLetters => the capability of having [A-Z] for username or password
+     * hasNumbers => the capability of having [0,1,2...9] for username or password
+     */
+    private fun readXmlAttr(typedAttributeSet: TypedArray) {
+        inputHint =
+            typedAttributeSet.getString(R.styleable.CustomInputField_hint) ?: ""
+        errorText =
+            typedAttributeSet.getString(R.styleable.CustomInputField_errorText) ?: "Please enter valid field data"
+        inputFieldType =
+            typedAttributeSet.getInt(R.styleable.CustomInputField_fieldType, -1)
+        errorColor =
+            typedAttributeSet.getColorStateList(R.styleable.CustomInputField_errorColor)
+                ?: ColorStateList.valueOf(Color.RED)
+        maxLength =
+            typedAttributeSet.getInt(R.styleable.CustomInputField_maxLength, 100)
+        minLength =
+            typedAttributeSet.getInt(R.styleable.CustomInputField_minLength, 1)
+        hasDot =
+            typedAttributeSet.getBoolean(R.styleable.CustomInputField_hasDot, false)
+        hasUnderScore =
+            typedAttributeSet.getBoolean(R.styleable.CustomInputField_hasUnderScore, false)
+        hasSpecialCharacter =
+            typedAttributeSet.getBoolean(R.styleable.CustomInputField_hasSpecialCharacter, false)
+        hasNumbers =
+            typedAttributeSet.getBoolean(R.styleable.CustomInputField_hasNumbers, false)
+        hasCapitalLetters =
+            typedAttributeSet.getBoolean(R.styleable.CustomInputField_hasCapitalLetters, false)
+    }
+
+    /**
+     * Set the error color for box border, error text, hint label and error icon view
+     */
+    private fun setBoxErrorColor(errorColor: ColorStateList?) {
+        boxStrokeErrorColor = errorColor
+        setErrorIconTintList(errorColor)
+        setErrorTextColor(errorColor)
+    }
+
+    /**
+     * Init the whole view
+     */
+    private fun init() {
+        addTextInputField()
+        setInputTextValidationListener()
+    }
+
+    /**
+     * Adding and input EditText to it
+     * setting the hint of EditText according to the xml value or with default value ""
+     */
+    private fun addTextInputField() {
+        inputEditText = AppCompatEditText(context)
+        inputEditText.hint = inputHint
+        setInputType()
+        addView(inputEditText)
+    }
+
+    /**
+     * Set input type according to the filed type
+     */
+    private fun setInputType() {
+        when (inputFieldType) {
+            // email input field
+            isEmailFieldType() -> inputEditText.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+            // password input field
+            isPasswordFieldType() -> inputEditText.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            // Check for the number input field
+            InputTypes.VISA.value,
+            InputTypes.MASTER_CARD.value,
+            InputTypes.AMEX.value,
+            InputTypes.CVV.value,
+            InputTypes.KSA_NATIONAL_ID.value,
+            InputTypes.EGY_NATIONAL_ID.value,
+            InputTypes.KSA_PHONE.value,
+            InputTypes.EGY_PHONE.value -> inputEditText.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_CLASS_NUMBER
+            // ip address input field
+            isWebFieldType() -> inputEditText.inputType =
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_WEB_EDIT_TEXT
+            // default input field (text)
+            else -> inputEditText.inputType =
+                InputType.TYPE_CLASS_TEXT
+        }
+    }
+
+    /**
+     * check if input field is an email field
+     */
+    private fun isEmailFieldType() = InputTypes.EMAIL.value
+
+    /**
+     * check if input field is an ip address field
+     */
+    private fun isWebFieldType() = InputTypes.IP_ADDRESS.value
+
+    /**
+     * check if input field is an password field
+     */
+    private fun isPasswordFieldType() = InputTypes.PASSWORD.value
+
+    /**
+     * Listing for EditText inputs and its validation process
+     */
+    private fun setInputTextValidationListener() {
+        inputEditText.doOnTextChanged { text, _, _, _ ->
+            Log.e("inputChange", "${text!!.isNotEmpty()}")
+            validateVIew(text)
+        }
+    }
+
+    /**
+     * Validate the input value
+     */
+    private fun validateVIew(text: CharSequence) {
+        error = when (inputFieldType) {
+            /**
+             * Validate input text as username
+             */
+            InputTypes.USERNAME.value -> if (ValidationUtil.isValidUserName(
+                    text.toString(),
+                    minLength,
+                    maxLength,
+                    hasDot,
+                    hasUnderScore,
+                    hasSpecialCharacter,
+                    hasNumbers,
+                    hasCapitalLetters
+                )
+            ) null else errorText
+            /**
+             * Validate input text as password
+             */
+            InputTypes.PASSWORD.value -> if (ValidationUtil.isValidUserPassword(
+                    text.toString(),
+                    minLength,
+                    maxLength,
+                    hasDot,
+                    hasUnderScore,
+                    hasSpecialCharacter,
+                    hasNumbers,
+                    hasCapitalLetters
+                )
+            ) null else errorText
+            /**
+             * Validate input text as email address
+             */
+            InputTypes.EMAIL.value -> if (ValidationUtil.isValidUserEmail(text.toString())) null else errorText
+            /**
+             * Validate input text as ksa phone number
+             */
+            InputTypes.KSA_PHONE.value -> if (ValidationUtil.isValidKsaPhoneNumber(text.toString())) null else errorText
+            /**
+             * Validate input text as ksa national Id
+             */
+            InputTypes.KSA_NATIONAL_ID.value -> if (ValidationUtil.isKsaCitizen(text.toString())) null else errorText
+            /**
+             * Validate input text as egyptian phone
+             */
+            InputTypes.EGY_PHONE.value -> if (ValidationUtil.isValidEgPhoneNumber(text.toString())) null else errorText
+            /**
+             * Validate input text as egyptian national Id
+             */
+            InputTypes.EGY_NATIONAL_ID.value -> if (ValidationUtil.isValidEgyptianNationalId(
+                    text.toString()
+                )
+            ) null else errorText
+            /**
+             * Validate input text as ip address
+             */
+            InputTypes.IP_ADDRESS.value -> if (ValidationUtil.isValidIpAddress(text.toString())) null else errorText
+            /**
+             * Validate input text as visa card
+             */
+            InputTypes.VISA.value -> if (ValidationUtil.isVisa(text.toString())) null else errorText
+            /**
+             * Validate input text as master card
+             */
+            InputTypes.MASTER_CARD.value -> if (ValidationUtil.isMaster(text.toString())) null else errorText
+            /**
+             * Validate input text as americam express card
+             */
+            InputTypes.AMEX.value -> if (ValidationUtil.isAmex(text.toString())) null else errorText
+            /**
+             * Validate input text as cvv number
+             */
+            InputTypes.CVV.value -> if (ValidationUtil.isValidCvvNumber(text.toString())) null else errorText
+            else -> null
+        }
+    }
+
+    /**
+     * enum to hold all types of input fields
+     */
+    private enum class InputTypes(val value: Int) {
+        USERNAME(0), PASSWORD(1), EMAIL(2), EGY_PHONE(3),
+        KSA_PHONE(4), KSA_NATIONAL_ID(5), EGY_NATIONAL_ID(6),
+        VISA(7), MASTER_CARD(8), AMEX(9), CVV(10), IP_ADDRESS(11);
+
+        companion object {
+            fun from(findValue: Int): InputTypes =
+                values().first { it.value == findValue }
+        }
+    }
+
+    /**
+     * Run the field validation from out side the view class and return bool
+     */
+    fun isValidView(): Boolean {
+        if (inputEditText.text.isNullOrEmpty()) error = errorText
+        return error == null
+    }
+}
---
+++ asgainputs/src/main/java/com/asgatech/asgainputs/utils/ValidationUtil.kt
@@ -0,0 +1,332 @@
+package com.asgatech.asgainputs.utils
+
+
+import android.util.Patterns
+
+/**
+ * @Author: Ahmed Saber
+ * @Date: 12/9/2021
+ * @Date: ahmedasga2593@gmail.com
+ */
+object ValidationUtil {
+    /**@usage:  validate entered username based on passed parameters.
+     * @param userName A String containing the user's name
+     * @param minLength An Integer value to determine username minimum length
+     * @param maxLength An Integer value to determine username maximum length
+     * @param hasDot A Boolean value to determine if username has at least one dot or not
+     * @param hasUnderScore A Boolean value to determine if username has at least one underscore or not
+     * @param hasSpecialCharacter A Boolean value to determine if username has at least special character from these [-+!@#$%^&*,?] or not
+     * @param haveNumbers A Boolean value to determine if username has at least one number in range 0-9 or not
+     * @param hasCapitalLetters A Boolean value to determine if username has at least one capital letter or not
+     * @return A Boolean value that is determined on given parameters
+     */
+    fun isValidUserName(
+        userName: String,
+        minLength: Int,
+        maxLength: Int,
+        hasDot: Boolean,
+        hasUnderScore: Boolean,
+        hasSpecialCharacter: Boolean,
+        haveNumbers: Boolean,
+        hasCapitalLetters: Boolean
+    ): Boolean {
+        return isValidInput(
+            userName,
+            minLength,
+            maxLength,
+            hasDot,
+            hasUnderScore,
+            hasSpecialCharacter,
+            haveNumbers,
+            hasCapitalLetters
+        )
+    }
+
+    /**@usage: validate entered user password based on passed parameters.
+     * @param userPassword A String containing the user's password
+     * @param minLength An Integer value to determine user password minimum length
+     * @param maxLength An Integer value to determine user password maximum length
+     * @param hasDot A Boolean value to determine if user password has at least one dot or not
+     * @param hasUnderScore A Boolean value to determine if user password has at least one underscore or not
+     * @param hasSpecialCharacter A Boolean value to determine if user password has at least special character from these [-+!@#$%^&*,?] or not
+     * @param haveNumbers A Boolean value to determine if user password has at least one number in range 0-9 or not
+     * @param hasCapitalLetters A Boolean value to determine if user password has at least one capital letter or not
+     * @return A Boolean value that is determined on given parameters
+     */
+    fun isValidUserPassword(
+        userPassword: String,
+        minLength: Int,
+        maxLength: Int,
+        hasDot: Boolean,
+        hasUnderScore: Boolean,
+        hasSpecialCharacter: Boolean,
+        haveNumbers: Boolean,
+        hasCapitalLetters: Boolean
+    ): Boolean {
+        return isValidInput(
+            userPassword,
+            minLength,
+            maxLength,
+            hasDot,
+            hasUnderScore,
+            hasSpecialCharacter,
+            haveNumbers,
+            hasCapitalLetters
+        )
+    }
+
+    /**@usage: validate entered input String based on passed parameters.
+     * @param inputString A String to validate based on values of given parameters
+     * @param minLength An Integer value to determine input String minimum length
+     * @param maxLength An Integer value to determine input String maximum length
+     * @param hasDot A Boolean value to determine if input String has at least one dot or not
+     * @param hasUnderScore A Boolean value to determine if input String has at least one underscore or not
+     * @param hasSpecialCharacter A Boolean value to determine if input String has at least special character from these [-+!@#$%^&*,?] or not
+     * @param haveNumbers A Boolean value to determine if input String has at least one number in range 0-9 or not
+     * @param hasCapitalLetters A Boolean value to determine if input String has at least one capital letter or not
+     * @return A Boolean value that is determined on given parameters
+     */
+    private fun isValidInput(
+        inputString: String,
+        minLength: Int,
+        maxLength: Int,
+        hasDot: Boolean,
+        hasUnderScore: Boolean,
+        hasSpecialCharacter: Boolean,
+        haveNumbers: Boolean,
+        hasCapitalLetters: Boolean
+    ): Boolean {
+        var regexStringBuilder = getInputRegexString(
+            minLength,
+            maxLength,
+            hasDot,
+            hasUnderScore,
+            hasSpecialCharacter,
+            haveNumbers,
+            hasCapitalLetters
+        )
+        var validationRegex = Regex(regexStringBuilder)
+        return inputString.matches(validationRegex)
+    }
+
+    /**@usage: build required regex based on passed parameters.
+     * @param minLength An Integer value to determine regex minimum length
+     * @param maxLength An Integer value to determine regex maximum length
+     * @param hasDot A Boolean value to determine if regex has at least one dot or not
+     * @param hasUnderScore A Boolean value to determine if regex has at least one underscore or not
+     * @param hasSpecialCharacter A Boolean value to determine if regex has at least special character from these [-+!@#$%^&*,?] or not
+     * @param haveNumbers A Boolean value to determine if regex has at least one number in range 0-9 or not
+     * @param hasCapitalLetters A Boolean value to determine if regex has at least one capital letter or not
+     * @return A String generated based on given parameters
+     */
+    private fun getInputRegexString(
+        minLength: Int,
+        maxLength: Int,
+        hasDot: Boolean,
+        hasUnderScore: Boolean,
+        hasSpecialCharacter: Boolean,
+        haveNumbers: Boolean,
+        hasCapitalLetters: Boolean
+    ): String {
+        var enableCapital = "!"
+        var enableDot = "!"
+        var enableUnderScore = "!"
+        var enableSpecialChar = "!"
+        var enableNumbers = "!"
+        if (hasCapitalLetters)
+            enableCapital = "="
+        if (hasDot)
+            enableDot = "="
+        if (hasUnderScore)
+            enableUnderScore = "="
+        if (hasSpecialCharacter)
+            enableSpecialChar = "="
+        if (haveNumbers)
+            enableNumbers = "="
+
+        return "^(?=.*[a-z])(?${enableCapital}.*[A-Z])(?${enableNumbers}.*\\d)(?${enableDot}.*[.])(?${enableUnderScore}.*[_])(?${enableSpecialChar}.*[-+!@#\$%^&*,?]).{${minLength},${maxLength}}$"
+    }
+
+    /**@usage: validate entered user email based on email address pattern imported from android.util.Patterns package.
+     * @param userEmail A String containing the user's email
+     * @return A Boolean value that is determined based on used pattern
+     */
+    fun isValidUserEmail(
+        userEmail: String
+    ): Boolean {
+        val emailPattern = Patterns.EMAIL_ADDRESS
+        return emailPattern.matcher(userEmail).matches()
+    }
+
+    /**@usage: validate entered Ksa phone number based on used regex.
+     * @param phoneNumber A String containing the user's ksa phone number
+     * @return A Boolean value that is determined based on used regex
+     */
+    fun isValidKsaPhoneNumber(phoneNumber: String): Boolean {
+        val ksaPhoneNumberRegex = Regex("^(009665|9665|\\+9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$")
+        return phoneNumber.matches(ksaPhoneNumberRegex)
+    }
+
+    /**@usage: validate entered Eg phone number based on used regex.
+     * @param phoneNumber A String containing the user's Eg phone number
+     * @return A Boolean value that is determined based on used regex
+     */
+    fun isValidEgPhoneNumber(phoneNumber: String): Boolean {
+        val egPhoneNumberRegex = Regex("^01[0-2,5]{1}[0-9]{8}$")
+        return phoneNumber.matches(egPhoneNumberRegex)
+    }
+
+    /**@usage: validate entered ip address based on used regex.
+     * @param ipAddress A String containing the ip address
+     * @return A Boolean value that is determined based on used regex
+     */
+    fun isValidIpAddress(ipAddress: String): Boolean {
+        var ipAddressRegex =
+            Regex("^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})(\\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]{1,2})){3}$")//simple one :: "^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$"
+        return ipAddress.matches(ipAddressRegex)
+    }
+
+    /**@usage: validate entered Eg nationalId based on used regex.
+     * @param nationalId A String containing the Eg nationalId
+     * @return A Boolean value that is determined based on used regex
+     */
+    fun isValidEgyptianNationalId(nationalId: String): Boolean {
+        var nationalIdRegex =
+            Regex("^(2|3)[0-9][1-9][0-1][1-9][0-3][1-9](01|02|03|04|11|12|13|14|15|16|17|18|19|21|22|23|24|25|26|27|28|29|31|32|33|34|35|88).+$")
+        return nationalId.matches(nationalIdRegex)
+    }
+
+    /**@usage: validate entered cvv number of credit card based on used regex.
+     * @param cvvNumber A String containing the cvv number
+     * @return A Boolean value that is determined based on used regex
+     */
+    fun isValidCvvNumber(cvvNumber: String): Boolean {
+        var cvvNumberRegex = Regex("^[0-9]{3,4}$")
+        return cvvNumber.matches(cvvNumberRegex)
+    }
+
+    /**@usage: validate entered credit Card Number is visa or not .
+     * @param creditCardNumber A String containing the credit Card number
+     * @return A Boolean value that is determined valid or not
+     */
+    fun isVisa(creditCardNumber: String): Boolean {
+        return getCreditCardType(creditCardNumber) == CreditCardType.VISA
+    }
+
+    /**@usage: validate entered credit Card Number is master or not .
+     * @param creditCardNumber A String containing the credit Card Number
+     * @return A Boolean value that is determined valid or not
+     */
+    fun isMaster(creditCardNumber: String): Boolean {
+        return getCreditCardType(creditCardNumber) == CreditCardType.MASTER
+    }
+
+    /**@usage: validate entered credit Card Number is american express or not .
+     * @param creditCardNumber A String containing the credit Card Number
+     * @return A Boolean value that is determined valid or not
+     */
+    fun isAmex(creditCardNumber: String): Boolean {
+        return getCreditCardType(creditCardNumber) == CreditCardType.AMEX
+    }
+
+    /**@usage: determine entered credit Card Number is visa or master or american express or not valid .
+     * @param creditCardNumber A String containing the credit Card Number
+     * @return A CreditCardType [VISA,MASTER,AMEX,NOT_VALID]
+     */
+    fun getCreditCardType(creditCardNumber: String): CreditCardType {
+        val visaRegex = Regex("^4[0-9]{12}(?:[0-9]{3})?\$")
+        val masterRegex = Regex("^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14})\$")
+        val amexRegex = Regex("^3[47][0-9]{13}\$")
+        return when {
+            creditCardNumber.matches(visaRegex) -> CreditCardType.VISA
+            creditCardNumber.matches(masterRegex) -> CreditCardType.MASTER
+            creditCardNumber.matches(amexRegex) -> CreditCardType.AMEX
+            else -> CreditCardType.NOT_VALID
+        }
+    }
+
+    /**@usage: validate entered identity number is saudi citizen or not .
+     * @param id A String containing the citizen identity number
+     * @return A Boolean value that is determined valid or not
+     */
+    fun isKsaCitizen(id: String): Boolean {
+        return getKsaNationalIdType(id) == IdType.SAUDI
+    }
+
+    /**@usage: validate entered identity number is resident citizen or not .
+     * @param id A String containing the citizen identity number
+     * @return A Boolean value that is determined valid or not
+     */
+    fun isKsaResident(id: String): Boolean {
+        return getKsaNationalIdType(id) == IdType.RESIDENT
+    }
+
+    /**@usage: determine entered identity number is saudi or resident or not valid .
+     * @param id A String containing the citizen identity number
+     * @return A IdType [SAUDI,RESIDENT,NOT_VALID]
+     */
+    fun getKsaNationalIdType(id: String): IdType {
+        val trimId = id.trim { it <= ' ' }
+        if (!trimId.matches("[0-9]+".toRegex())) {
+            return IdType.NOT_VALID
+        }
+        if (trimId.length != 10) {
+            return IdType.NOT_VALID
+        }
+        val type = Integer.parseInt(trimId.substring(0, 1))
+        if (type != 2 && type != 1) {
+            return IdType.NOT_VALID
+        }
+        var sum = 0
+        for (i in 0..9) {
+            sum += if (i % 2 == 0) {
+                val zfOdd = "%02d".format(Integer.parseInt(trimId.substring(i, i + 1)) * 2)
+                Integer.parseInt(zfOdd.substring(0, 1)) + Integer.parseInt(zfOdd.substring(1, 2))
+            } else {
+                Integer.parseInt(trimId.substring(i, i + 1))
+            }
+        }
+        return if (sum % 10 != 0) IdType.NOT_VALID else IdType.from(type)
+    }
+
+    /**
+     * @usage: this enum class identifies different types of identity number
+     */
+    enum class IdType(val value: Int) {
+        SAUDI(1), RESIDENT(2), NOT_VALID(-1);
+
+        companion object {
+            fun from(findValue: Int): IdType = IdType.values().first { it.value == findValue }
+        }
+    }
+
+    /**
+     * @usage: this enum class identifies different types of credit card
+     */
+    enum class CreditCardType(val value: Int) {
+        VISA(1), MASTER(2), AMEX(3), NOT_VALID(-1);
+
+        companion object {
+            fun from(findValue: Int): CreditCardType =
+                CreditCardType.values().first { it.value == findValue }
+        }
+    }
+}
+
+fun main() {
+    println(ValidationUtil.isValidUserName("Ahd._@8", 5, 7, true, true, true, true, true))
+    println(ValidationUtil.isValidKsaPhoneNumber("+966544949955"))
+    println(ValidationUtil.isValidEgPhoneNumber("01520713678"))
+    println(ValidationUtil.isValidIpAddress("192.168.1.1"))
+    println(ValidationUtil.isValidEgyptianNationalId("293050232190031"))
+    println(ValidationUtil.isValidCvvNumber("531"))
+    println(ValidationUtil.isVisa("4111111111111111"))
+    println(ValidationUtil.isMaster("5105105105105100"))
+    println(ValidationUtil.isAmex("371449635398431"))
+    println(ValidationUtil.getKsaNationalIdType("1635788548"))
+    println(ValidationUtil.getKsaNationalIdType("2827927043"))
+    println(ValidationUtil.getKsaNationalIdType("293050232190031"))
+    println(ValidationUtil.isKsaCitizen("1635788548"))
+    println(ValidationUtil.isKsaResident("2827927043"))
+    println(ValidationUtil.isKsaResident("293050232190031"))
+}
